99725669b45f1fc845817cd6f2c01abef1bb8c93
samkeen <sam.keen@nike.com>
2014-10-16 22:10:46 -0700

    TMP COMMIT

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,29 @@ var express = require('express');
 var router = express.Router();
 var AWS = require('aws-sdk');
 
+
+router.get("/listing/:bucketName", function (req, res) {
+
+  var params = {
+    Bucket: req.params.bucketName
+    //,
+    //Prefix: '*index.html'
+      ,Marker: 'stage/PreBlueGreen/SyncService/SYNC-REL-91_2014-10-16T11:13:21PDT/HTML_Reports/LSS_Delete_Running_Activity.html'
+  };
+
+  AWS.config.loadFromPath('./config/aws.config.json');
+  var s3 = new AWS.S3();
+
+  s3.listObjects(params, function(err, data) {
+    if (err) console.log(err, err.stack); // an error occurred
+    else     console.log(data);           // successful response
+  });
+
+  res.end();
+
+
+});
+
 /* GET home page. */
 router.get(/^\/([^\/]*)\/(.+)/, function (req, res) {
 
